e0aa135a7459e3eff1a64d7649f787c777ead5a4
hack cloudnative <hackcloudnative@gmail.com>
2022-12-15 13:42:07 +0000

    updated changes

--- page/machine_homepage.php
+++ page/machine_homepage.php
@@ -1,13 +1,18 @@
 <?php
+include_once $_SERVER["DOCUMENT_ROOT"].'/project/module/conn.php';
 /*
 Machine name
 Button to start machine
 Machine Disc
 */
-session_start()
-if (is_set($_GET["name"])){
-    echo "Machine name: ".$name;
+session_start();
+if(isset($_GET["name"])){
+    // GET machine name
+    $query = "select * from machines_db where machine_name='".$_GET["name"]."'";
+    $machine_data = mysqli_fetch_array($conn->query($query));
+    echo $machine_data["machine_name"];
+    echo "<br>Logo: ".$machine_data['machine_logo_url'];
     // BUTTON HERE
-    echo "Machine notes: ".$disc;
+    echo "<br>Machine notes: ".$machine_data["machine_disc"];
 }
 ?>
