a72a6ecd0405ad3763ba114faa232c9df60e91cb
hack cloudnative <hackcloudnative@gmail.com>
2022-12-16 11:28:45 +0000

    commit

--- page/support.php
+++ page/support.php
@@ -1,3 +1,31 @@
+<head>
+<style>
+.grid-container {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  background-color: #2196F3;
+  padding: 10px;
+}
+.grid-item {
+  background-color: rgba(255, 255, 255, 0.8);
+  border: 1px solid rgba(0, 0, 0, 0.8);
+  padding: 20px;
+  font-size: 30px;
+  text-align: center;
+}
+</style>
+</head>
+
 <?php
-// List all support tickets
+include_once $_SERVER["DOCUMENT_ROOT"].'/project/module/conn.php';
+session_start();
 ?>
+<div class="grid-container">
+<?php
+$query = "select * from support where user='".$_SESSION['name']."';";
+$result = mysqli_query($conn, $query);
+while($row = mysqli_fetch_array($result)) { ?>
+    <div class="grid-item"><?php echo $row["question"]; ?></div>
+    <div class="grid-item"><?php echo $row["answer"]; ?></div>
+    <div class="grid-item" id="<?php echo $row['id']; ?>"><button>Delete</button></div> 
+<?php } ?></div>
