34ee6eea3c6c083049838502530f150e2c0806ff
srikar reddy <srikarpy11@gmail.com>
2022-12-17 03:55:04 +0530

    new css changes to login page

--- page/login.php
+++ page/login.php
@@ -1,38 +1,57 @@
 <?php
 include $_SERVER["DOCUMENT_ROOT"].'/project/module/conn.php';
-
-
-// Report simple running errors
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
-
-// Reporting E_NOTICE can be good too (to report uninitialized
-// variables or catch variable name misspellings ...)
-error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
-
-// Report all errors except E_NOTICE
-// This is the default value set in php.ini
-error_reporting(E_ALL ^ E_NOTICE);
-
-// For PHP >=5.3 use: E_ALL & ~E_NOTICE
-
-// Report all PHP errors (see changelog)
-error_reporting(E_ALL);
-
-// Report all PHP errors
-error_reporting(-1);
-
-// Same as error_reporting(E_ALL);
-ini_set('error_reporting', E_ALL);
-
+include $_SERVER["DOCUMENT_ROOT"].'/project/module/sessionmgr.php';
 
 # If GET - show login page
 if ($_SERVER['REQUEST_METHOD'] == "GET"){ ?>
-<form method="POST" action="/project/page/login.php">
-<label>User Name</label>
-<input type="text" name="username" placeholder="User Name"><br>
-<label>Password</label>
-<input type="password" name="password" placeholder="Password"><br> 
-<button type="submit">Login</button>
+<link
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+  rel="stylesheet"
+  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+  crossorigin="anonymous"
+/>
+<section class="vh-100 gradient-custom">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-dark text-white" style="border-radius: 1rem">
+          <div class="card-body p-5 text-center">
+            <form method="POST" action="/project/page/login.php">
+              <div class="mb-md-5 mt-md-4 pb-5">
+                <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                <p class="text-white-50 mb-5">
+                  Please enter your Username and password!
+                </p>
+
+                <div class="form-outline form-white mb-4">
+                  <input
+                    type="text"
+                    name="username"
+                    class="form-control form-control-lg"
+                  />
+                  <label class="form-label" for="typeEmailX">Username</label>
+                </div>
+
+                <div class="form-outline form-white mb-4">
+                  <input
+                    name="password"
+                    type="password"
+                    class="form-control form-control-lg"
+                  />
+                  <label class="form-label" for="typePasswordX">Password</label>
+                </div>
+
+                <button class="btn btn-outline-light btn-lg px-5" type="submit">
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 <?php }
 
 # If POST process inputs
@@ -43,13 +62,9 @@ if (mysqli_fetch_array($conn->query($query)) == NULL){
 	}
 else {
 		session_start();
-		$_SESSION['name']=$_POST["username"];
-		$query = 'select role from rbac where username="'.$_POST["username"].'";';
-		// GET role of user
-		$_SESSION['role'] = mysqli_fetch_array($conn->query($query))["role"];
-
-		//header("Location: http://localhost/project/page/dashboard.php");
-		header("Location: http://localhost/project/page/dashboard-gui.php");
+		$_SESSION['name']="JACK";
+		header("Location: http://localhost/project/page/dashboard.php");
+		//var_dump($_SESSION['name']);
 	}
 }
 ?>
