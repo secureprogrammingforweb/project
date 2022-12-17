f3cfcd221790af0f41b8588e99043b40c2b42625
hack cloudnative <hackcloudnative@gmail.com>
2022-12-17 23:56:18 +0000

    csrf modification

--- page/login.php
+++ page/login.php
@@ -1,7 +1,7 @@
 <?php
 
 include $_SERVER["DOCUMENT_ROOT"].'/project/module/conn.php';
-include $_SERVER["DOCUMENT_ROOT"].'/project/module/security_header.php.php';
+include $_SERVER["DOCUMENT_ROOT"].'/project/module/security_headers.php.php';
 
 $password = true;
 
